8b98ec2a21edaf1b06ce4084477f8c35920ddb20
Dibran Mulder <dibran.mulder@outlook.com>
2025-04-24 09:45:28 +0200

    added missing files

---
+++ server/redis.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisSentinelConfig struct {
+	SentinelHost     string `json:"sentinel_host"`
+	SentinelPort     int    `json:"sentinel_port"`
+	Password         string `json:"password"`
+	MasterName       string `json:"master_name"`
+	SentinelUsername string `json:"sentinel_username"`
+}
+
+type RedisConfig struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	Password string `json:"password"`
+}
+
+func NewRedisClient(config *RedisConfig) (*redis.Client, error) {
+	ctx := context.Background()
+	addr := fmt.Sprintf("%v:%v", config.Host, config.Port)
+	options := &redis.Options{
+		Addr:     addr,
+		Password: config.Password,
+	}
+	client := redis.NewClient(options)
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to Redis: %v", err)
+	}
+
+	return client, err
+}
+
+func NewRedisSentinelClient(config *RedisSentinelConfig) (*redis.Client, error) {
+	ctx := context.Background()
+
+	addr := fmt.Sprintf("%v:%v", config.SentinelHost, config.SentinelPort)
+	sentinelOptions := &redis.FailoverOptions{
+		MasterName:       config.MasterName,
+		SentinelAddrs:    []string{addr},
+		Password:         config.Password,
+		SentinelUsername: config.SentinelUsername,
+	}
+
+	client := redis.NewFailoverClient(sentinelOptions)
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to Redis through Sentinel: %w", err)
+	}
+
+	return client, err
+}
